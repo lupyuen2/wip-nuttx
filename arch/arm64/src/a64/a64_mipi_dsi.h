5f48491efc071baa62290a2e477ed4630082fcf5
Lee Lup Yuen <luppy@appkaki.com>
2022-12-09 21:35:53 +0800

    Clean up

--- arch/arm64/src/a64/a64_mipi_dsi.h
+++ arch/arm64/src/a64/a64_mipi_dsi.h
@@ -1,4 +1,27 @@
 /****************************************************************************
+ * arch/arm64/src/a64/a64_mipi_dphy.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __ARCH_ARM64_SRC_A64_A64_MIPI_DSI_H
+#define __ARCH_ARM64_SRC_A64_A64_MIPI_DSI_H
+
+/****************************************************************************
  * Included Files
  ****************************************************************************/
 
@@ -32,3 +55,5 @@ int a64_mipi_dsi_enable(void);
 /// Start MIPI DSI HSC and HSD. (High Speed Clock Mode and High Speed Data Transmission)
 /// Based on https://lupyuen.github.io/articles/dsi#appendix-start-mipi-dsi-hsc-and-hsd
 int a64_mipi_dsi_start(void);
+
+#endif /* __ARCH_ARM64_SRC_A64_A64_MIPI_DSI_H */
