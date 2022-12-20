98d694dfad2d5e8a8be561b604e52b0bb9af730e
Lee Lup Yuen <luppy@appkaki.com>
2022-12-20 18:13:09 +0800

    Adding RSB

--- arch/arm64/src/a64/a64_rsb.c
+++ arch/arm64/src/a64/a64_rsb.c
@@ -49,35 +49,35 @@
  ****************************************************************************/
 
 /// TODO: Reduced Serial Bus Base Address (R_RSB) (A64 Page 75)
-const R_RSB_BASE_ADDRESS = 0x01f03400;
+#define R_RSB_BASE_ADDRESS 0x01f03400
 
 /* A64 Reduced Serial Bus Registers and Bit Definitions *********************/
 
 // RSB Control Register (RSB_CTRL) (A80 Page 923)
-const RSB_CTRL   = 0x00;  // RSB Control Register
+#define RSB_CTRL   (R_RSB_BASE_ADDRESS + 0x00)
 
 // RSB Status Register (RSB_STAT) (A80 Page 924)
-const RSB_STAT   = 0x0c;  // RSB Status Register
+#define RSB_STAT   (R_RSB_BASE_ADDRESS + 0x0c)
 
 // RSB Address Register (RSB_AR) (A80 Page 926)
-const RSB_AR     = 0x10;  // RSB Address Register
+#define RSB_AR     (R_RSB_BASE_ADDRESS + 0x10)
 
 // RSB Data Buffer Register (RSB_DATA) (A80 Page 926)
-const RSB_DATA   = 0x1c;  // RSB Data Buffer Register
+#define RSB_DATA   (R_RSB_BASE_ADDRESS + 0x1c)
 
 // RSB Command Register (RSB_CMD) (A80 Page 928)
-const RSB_CMD    = 0x2c;  // RSB Command Register
+#define RSB_CMD    (R_RSB_BASE_ADDRESS + 0x2c)
 
 // RSB Device Address Register (RSB_DAR) (A80 Page 928)
-const RSB_DAR    = 0x30;  // RSB Device Address Register
+#define RSB_DAR    (R_RSB_BASE_ADDRESS + 0x30)
 
 /* A64 Reduced Serial Bus Commands ******************************************/
 
 /// Read a byte from Reduced Serial Bus (A80 Page 918)
-const RSBCMD_RD8 = 0x8B;
+#define RSBCMD_RD8 0x8B
 
 /// Write a byte to Reduced Serial Bus (A80 Page 918)
-const RSBCMD_WR8 = 0x4E;
+#define RSBCMD_WR8 0x4E
 
 /****************************************************************************
  * Private Functions
@@ -93,8 +93,8 @@ static int rsb_wait_trans(void)
       // RSB Control Register (RSB_CTRL) (A80 Page 923)
       // At RSB Offset 0x0000
       // Wait for START_TRANS (Bit 7) to be 0 (Transaction Completed or Error)
-      const reg = getreg32(R_RSB_BASE_ADDRESS + RSB_CTRL); 
-      if (reg & (1 << 7) == 0)
+      uint32_t reg = getreg32(RSB_CTRL); 
+      if ((reg & (1 << 7)) == 0)
         { 
           break; 
         }
@@ -115,7 +115,7 @@ static int rsb_wait_trans(void)
 static int rsb_wait_status(void)
 {
   // Wait for transaction to complete or fail with error
-  int ret = rsb_wait_trans(desc);
+  int ret = rsb_wait_trans();
   if (ret != 0)
     {
       return ret;
@@ -124,7 +124,7 @@ static int rsb_wait_status(void)
   // RSB Status Register (RSB_STAT) (A80 Page 924)
   // At RSB Offset 0x000c
   // If TRANS_OVER (Bit 0) is 1, then RSB Transfer has completed without error
-  uint32_t reg = getreg32(R_RSB_BASE_ADDRESS + RSB_STAT);
+  uint32_t reg = getreg32(RSB_STAT);
   if (reg == 0x01)
     {
       return 0;
@@ -149,32 +149,32 @@ int a64_rsb_read(
   // At RSB Offset 0x002C
   // Set to 0x8B (RD8) to read one byte
   _info("rsb_read: rt_addr=0x%x, reg_addr=0x%x\n", rt_addr, reg_addr);  // TODO
-  putreg32(RSBCMD_RD8, R_RSB_BASE_ADDRESS + RSB_CMD);   
+  putreg32(RSBCMD_RD8, RSB_CMD);   
 
   // RSB Device Address Register (RSB_DAR) (A80 Page 928)
   // At RSB Offset 0x0030
   // Set RTA (Bits 16 to 23) to the Run-Time Address (0x2D for AXP803 PMIC)
   uint32_t rta = rt_addr << 16;
-  putreg32(rta, R_RSB_BASE_ADDRESS + RSB_DAR);   
+  putreg32(rta, RSB_DAR);   
 
   // RSB Address Register (RSB_AR) (A80 Page 926)
   // At RSB Offset 0x0010
   // Set to the Register Address that we’ll read from AXP803 PMIC
-  putreg32(reg_addr, R_RSB_BASE_ADDRESS + RSB_AR);    
+  putreg32(reg_addr, RSB_AR);    
 
   // RSB Control Register (RSB_CTRL) (A80 Page 923)
   // At RSB Offset 0x0000
   // Set START_TRANS (Bit 7) to 1 (Start Transaction)
-  putreg32(0x80, R_RSB_BASE_ADDRESS + RSB_CTRL);  
+  putreg32(0x80, RSB_CTRL);  
 
   // Wait for RSB Status
-  int ret = rsb_wait_stat("Read RSB");
+  int ret = rsb_wait_status();
   if (ret != 0) { return ret; }
 
   // RSB Data Buffer Register (RSB_DATA) (A80 Page 926)
   // At RSB Offset 0x001c
   // Contains the Register Value read from AXP803 PMIC
-  return getreg8(R_RSB_BASE_ADDRESS + RSB_DATA);
+  return getreg8(RSB_DATA);
 }
 
 /// Write a byte to Reduced Serial Bus.
@@ -189,29 +189,29 @@ int a64_rsb_write(
   // At RSB Offset 0x002C
   // Set to 0x4E (WR8) to write one byte
   _info("rt_addr=0x%x, reg_addr=0x%x, value=0x%x\n", rt_addr, reg_addr, value);  // TODO
-  putreg32(RSBCMD_WR8, R_RSB_BASE_ADDRESS + RSB_CMD);   
+  putreg32(RSBCMD_WR8, RSB_CMD);   
 
   // RSB Device Address Register (RSB_DAR) (A80 Page 928)
   // At RSB Offset 0x0030
   // Set RTA (Bits 16 to 23) to the Run-Time Address (0x2D for AXP803 PMIC)
   uint32_t rta = rt_addr << 16;
-  putreg32(rta, R_RSB_BASE_ADDRESS + RSB_DAR);   
+  putreg32(rta, RSB_DAR);   
 
   // RSB Address Register (RSB_AR) (A80 Page 926)
   // At RSB Offset 0x0010
   // Set to the Register Address that we’ll write to AXP803 PMIC
-  putreg32(reg_addr, R_RSB_BASE_ADDRESS + RSB_AR);    
+  putreg32(reg_addr, RSB_AR);    
 
   // RSB Data Buffer Register (RSB_DATA) (A80 Page 926)
   // At RSB Offset 0x001c
   // Set to the Register Value that will be written to AXP803 PMIC
-  putreg32(value, R_RSB_BASE_ADDRESS + RSB_DATA);  
+  putreg32(value, RSB_DATA);  
 
   // RSB Control Register (RSB_CTRL) (A80 Page 923)
   // At RSB Offset 0x0000
   // Set START_TRANS (Bit 7) to 1 (Start Transaction)
-  putreg32(0x80, R_RSB_BASE_ADDRESS + RSB_CTRL);  
+  putreg32(0x80, RSB_CTRL);  
 
   // Wait for RSB Status
-  return rsb_wait_stat();
+  return rsb_wait_status();
 }
