2afdd5b6d8d5daafd5840124fb543e7a80d016ca
Lee Lup Yuen <luppy@appkaki.com>
2023-01-03 19:55:10 +0800

    Read touch event OK yay!

--- boards/arm64/a64/pinephone/src/pinephone_bringup.c
+++ boards/arm64/a64/pinephone/src/pinephone_bringup.c
@@ -258,12 +258,19 @@ void touch_panel_initialize(void)
 // Configure for GPIO Input
 #define CTP_INT (PIO_INPUT | PIO_PORT_PIOH | PIO_PIN4)
 
+#define CTP_FREQ 400000  // I2C Frequency: 400 kHz
+#define CTP_I2C_ADDR 0x5d  // Default I2C Address for Goodix GT917S
+
 static void touch_panel_read(struct i2c_master_s *i2c);
 static int touch_panel_i2c_read(
   struct i2c_master_s *i2c,  // I2C Bus
   uint16_t reg,  // I2C Register
   uint8_t *buf,  // Receive Buffer
   size_t buflen  // Receive Buffer Size
+);
+static int touch_panel_set_status(
+  struct i2c_master_s *i2c,  // I2C Bus
+  uint8_t status  // Status value to be set
 );
 
 // Poll for Touch Panel Interrupt (PH4) by reading as GPIO Input
@@ -327,15 +334,18 @@ static void touch_panel_read(struct i2c_master_s *i2c)
   touch_panel_i2c_read(i2c, GOODIX_REG_ID, id, sizeof(id));
   // Shows "39 31 37 53" or "917S"
 
-  // Request for Touch Coordinates
+  // Read the Touch Panel Status
   uint8_t req[1];
   touch_panel_i2c_read(i2c, GOODIX_READ_COORD_ADDR, req, sizeof(req));
-  // Shows "80"
+  // Shows "81"
 
   // Read the Touch Coordinates
   uint8_t touch[6];
   touch_panel_i2c_read(i2c, GOODIX_POINT1_X_ADDR, touch, sizeof(touch));
-  // Shows "80 00 00 00 00 00"
+  // Shows "9f 01 31 02 2f 00"
+
+  // Set the Touch Panel Status to 0
+  touch_panel_set_status(i2c, 0);
 }
 
 static int touch_panel_i2c_read(
@@ -344,9 +354,12 @@ static int touch_panel_i2c_read(
   uint8_t *buf,  // Receive Buffer
   size_t buflen  // Receive Buffer Size
 ) {
-  uint32_t freq = 400000;  // 400 kHz
-  uint16_t addr = 0x5d;  // Default I2C Address for Goodix GT917S
-  uint8_t regbuf[2] = { reg >> 8, reg & 0xff };  // Flip the bytes
+  uint32_t freq = CTP_FREQ;  // 400 kHz
+  uint16_t addr = CTP_I2C_ADDR;  // Default I2C Address for Goodix GT917S
+  uint8_t regbuf[2] = {
+    reg >> 8,   // Swap the bytes
+    reg & 0xff  // Swap the bytes
+  };
 
   // Erase the receive buffer
   memset(buf, 0xff, buflen);
@@ -371,7 +384,8 @@ static int touch_panel_i2c_read(
   };
 
   // Execute the I2C Transfer
-  int ret = I2C_TRANSFER(i2c, msgv, 2);
+  const int msgv_len = sizeof(msgv) / sizeof(msgv[0]);
+  int ret = I2C_TRANSFER(i2c, msgv, msgv_len);
   if (ret < 0) { _err("I2C Error: %d\n", ret); return ret; }
 
   // Dump the receive buffer
@@ -379,229 +393,36 @@ static int touch_panel_i2c_read(
   return OK;
 }
 
-///////////////////////////////////////////////////////////////////////////////
+static int touch_panel_set_status(
+  struct i2c_master_s *i2c,  // I2C Bus
+  uint8_t status  // Status value to be set
+) {
+  uint16_t reg = GOODIX_READ_COORD_ADDR;  // I2C Register
+  uint32_t freq = CTP_FREQ;  // 400 kHz
+  uint16_t addr = CTP_I2C_ADDR;  // Default I2C Address for Goodix GT917S
+  uint8_t buf[3] = {
+    reg >> 8,    // Swap the bytes
+    reg & 0xff,  // Swap the bytes
+    status
+  };
 
-static uint8_t GT911_Config[] = {
-		0x81, 0x00, 0x04, 0x58, 0x02, 0x0A, 0x0C, 0x20, 0x01, 0x08, 0x28, 0x05, 0x50, // 0x8047 - 0x8053
-		0x3C, 0x0F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x8054 - 0x8060
-		0x00, 0x89, 0x2A, 0x0B, 0x2D, 0x2B, 0x0F, 0x0A, 0x00, 0x00, 0x01, 0xA9, 0x03, // 0x8061 - 0x806D
-		0x2D, 0x00, 0x01, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x21, // 0x806E - 0x807A
-		0x59, 0x94, 0xC5, 0x02, 0x07, 0x00, 0x00, 0x04, 0x93, 0x24, 0x00, 0x7D, 0x2C, // 0x807B - 0x8087
-		0x00, 0x6B, 0x36, 0x00, 0x5D, 0x42, 0x00, 0x53, 0x50, 0x00, 0x53, 0x00, 0x00, // 0x8088	- 0x8094
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x8095 - 0x80A1
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x80A2 - 0x80AD
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x04, 0x06, 0x08, 0x0A, // 0x80AE - 0x80BA
-		0x0C, 0x0E, 0x10, 0x12, 0x14, 0x16, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, // 0x80BB - 0x80C7
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x80C8 - 0x80D4
-		0x02, 0x04, 0x06, 0x08, 0x0A, 0x0F, 0x10, 0x12, 0x16, 0x18, 0x1C, 0x1D, 0x1E, // 0x80D5 - 0x80E1
-		0x1F, 0x20, 0x21, 0x22, 0x24, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, // 0x80E2 - 0x80EE
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 0x80EF - 0x80FB
-		0x00, 0x00, 0xD6, 0x01 }; // 0x80FC - 0x8100
-static GT911_Status_t CommunicationResult;
-static uint8_t TxBuffer[200];
-static uint8_t RxBuffer[200];
+  // Compose the I2C Message
+  struct i2c_msg_s msgv[1] =
+  {
+    {
+      .frequency = freq,
+      .addr      = addr,
+      .flags     = 0,
+      .buffer    = buf,
+      .length    = sizeof(buf)
+    }
+  };
 
-static void GT911_Reset(void);
-static void GT911_CalculateCheckSum(void);
-static GT911_Status_t GT911_SetCommandRegister(uint8_t command);
-static GT911_Status_t GT911_GetProductID(uint32_t* id);
-static GT911_Status_t GT911_SendConfig(void);
-static GT911_Status_t GT911_GetStatus(uint8_t* status);
-static GT911_Status_t GT911_SetStatus(uint8_t status);
-/* API Implementation --------------------------------------------------------*/
-GT911_Status_t GT911_Init(GT911_Config_t config){
-//	Set X resolution
-	GT911_Config[1] = config.X_Resolution & 0x00FF;
-	GT911_Config[2] = (config.X_Resolution >> 8) & 0x00FF;
-//	Set Y resolution
-	GT911_Config[3] = config.Y_Resolution & 0x00FF;
-	GT911_Config[4] = (config.Y_Resolution >> 8) & 0x00FF;
-//  Set touch number
-	GT911_Config[5] = config.Number_Of_Touch_Support;
-//  set reverse Y
-	GT911_Config[6] = 0;
-	GT911_Config[6] |= config.ReverseY << 7;
-//  set reverse X
-	GT911_Config[6] |= config.ReverseX << 6;
-//  set switch X2Y
-	GT911_Config[6] |= config.SwithX2Y << 3;
-//  set Sito
-	GT911_Config[6] |= config.SoftwareNoiseReduction << 2;
-
-	//Reset chip
-	GT911_Reset();
-	//Get product ID
-	uint32_t productID = 0;
-	CommunicationResult = GT911_GetProductID(&productID);
-	if(CommunicationResult != GT911_OK){
-		return CommunicationResult;
-	}
-	if(productID == 0){
-		return GT911_NotResponse;
-	}
-	//Reset chip
-	GT911_Reset();
-	CommunicationResult = GT911_SendConfig();
-	if(CommunicationResult != GT911_OK){
-		return CommunicationResult;
-	}
-	GT911_SetCommandRegister(0x00);
-	return GT911_OK;
+  // Execute the I2C Transfer
+  const int msgv_len = sizeof(msgv) / sizeof(msgv[0]);
+  int ret = I2C_TRANSFER(i2c, msgv, msgv_len);
+  if (ret < 0) { _err("I2C Error: %d\n", ret); return ret; }
+  return OK;
 }
-
-GT911_Status_t GT911_ReadTouch(TouchCordinate_t *cordinate, uint8_t *number_of_cordinate) {
-	uint8_t StatusRegister;
-	GT911_Status_t Result = GT911_NotResponse;
-	Result = GT911_GetStatus(&StatusRegister);
-	if (Result != GT911_OK) {
-		return Result;
-	}
-	if ((StatusRegister & 0x80) != 0) {
-		*number_of_cordinate = StatusRegister & 0x0F;
-		if (*number_of_cordinate != 0) {
-			for (uint8_t i = 0; i < *number_of_cordinate; i++) {
-				TxBuffer[0] = ((GOODIX_POINT1_X_ADDR + (i* 8)) & 0xFF00) >> 8;
-				TxBuffer[1] = (GOODIX_POINT1_X_ADDR + (i* 8)) & 0xFF;
-				GT911_I2C_Write(GOODIX_ADDRESS, TxBuffer, 2);
-				GT911_I2C_Read(GOODIX_ADDRESS, RxBuffer, 6);
-				cordinate[i].x = RxBuffer[0];
-				cordinate[i].x = (RxBuffer[1] << 8) + cordinate[i].x;
-				cordinate[i].y = RxBuffer[2];
-				cordinate[i].y = (RxBuffer[3] << 8) + cordinate[i].y;
-			}
-		}
-		GT911_SetStatus(0);
-	}
-	return GT911_OK;
-}
-
-//Private functions Implementation ---------------------------------------------------------*/
-static void GT911_Reset(void){
-	GT911_INT_Input();
-	GT911_RST_Control(0);
-	GT911_Delay(20);
-	GT911_INT_Control(0);
-	GT911_Delay(50);
-	GT911_RST_Control(1);
-	GT911_Delay(100);
-	GT911_INT_Output();
-	GT911_Delay(100);
-}
-
-static void GT911_CalculateCheckSum(void){
-	GT911_Config[184] = 0;
-	for(uint8_t i = 0 ; i < 184 ; i++){
-		GT911_Config[184] += GT911_Config[i];
-	}
-	GT911_Config[184] = (~GT911_Config[184]) + 1;
-}
-
-static GT911_Status_t GT911_SetCommandRegister(uint8_t command){
-	TxBuffer[0] = (GOODIX_REG_COMMAND & 0xFF00) >> 8;
-	TxBuffer[1] = GOODIX_REG_COMMAND & 0xFF;
-	TxBuffer[2] = command;
-	return GT911_I2C_Write(GOODIX_ADDRESS, TxBuffer, 3);
-}
-
-static GT911_Status_t GT911_GetProductID(uint32_t* id){
-	TxBuffer[0] = (GOODIX_REG_ID & 0xFF00) >> 8;
-	TxBuffer[1] = GOODIX_REG_ID & 0xFF;
-	GT911_Status_t Result = GT911_NotResponse;
-	Result = GT911_I2C_Write(GOODIX_ADDRESS, TxBuffer, 2);
-	if(Result == GT911_OK){
-		Result = GT911_I2C_Read(GOODIX_ADDRESS, RxBuffer, 4);
-		if( Result == GT911_OK){
-			memcpy(id, RxBuffer, 4);
-		}
-	}
-	return Result;
-}
-
-static GT911_Status_t GT911_SendConfig(void){
-	GT911_CalculateCheckSum();
-	TxBuffer[0] = (GOODIX_REG_CONFIG_DATA & 0xFF00) >> 8;
-	TxBuffer[1] = GOODIX_REG_CONFIG_DATA & 0xFF;
-	memcpy(&TxBuffer[2], GT911_Config, sizeof(GT911_Config));
-	return GT911_I2C_Write(GOODIX_ADDRESS, TxBuffer, sizeof(GT911_Config) + 2);
-}
-
-static GT911_Status_t GT911_GetStatus(uint8_t* status){
-	TxBuffer[0] = (GOODIX_READ_COORD_ADDR & 0xFF00) >> 8;
-	TxBuffer[1] = GOODIX_READ_COORD_ADDR & 0xFF;
-	GT911_Status_t Result = GT911_NotResponse;
-	Result = GT911_I2C_Write(GOODIX_ADDRESS, TxBuffer, 2);
-	if(Result == GT911_OK){
-		Result = GT911_I2C_Read(GOODIX_ADDRESS, RxBuffer, 1);
-		if( Result == GT911_OK){
-			*status = RxBuffer[0];
-		}
-	}
-	return Result;
-}
-
-static GT911_Status_t GT911_SetStatus(uint8_t status){
-	TxBuffer[0] = (GOODIX_READ_COORD_ADDR & 0xFF00) >> 8;
-	TxBuffer[1] = GOODIX_READ_COORD_ADDR & 0xFF;
-	TxBuffer[2] = status;
-	return GT911_I2C_Write(GOODIX_ADDRESS, TxBuffer, 3);
-}
-
-#ifdef NOTUSED
-struct goodix_point_t {
-	int id;
-	int x;
-	int y;
-	int w;
-	int p;
-	int tool_type;
-};
-
-struct goodix_config_data {
-	int length;
-	u8 data[GTP_CONFIG_MAX_LENGTH + GTP_ADDR_LENGTH];
-};
-
-struct goodix_ts_platform_data {
-	int irq_gpio;
-	int rst_gpio;
-	u32 irq_flags;
-	u32 abs_size_x;
-	u32 abs_size_y;
-	u32 max_touch_id;
-	u32 max_touch_width;
-	u32 max_touch_pressure;
-	u32 key_map[MAX_KEY_NUMS];
-	u32 key_nums;
-	u32 int_sync;
-	u32 driver_send_cfg;
-	u32 swap_x2y;
-	u32 slide_wakeup;
-	u32 auto_update;
-	u32 auto_update_cfg;
-	u32 esd_protect;
-	u32 type_a_report;
-	u32 power_off_sleep;
-	u32 resume_in_workqueue;
-	u32 pen_suppress_finger;
-	struct goodix_config_data config;
-};
-
-static void touch_panel_setup(void)
-{
-	/* set parameters at here if you platform doesn't DTS */
-	pdata->rst_gpio = GTP_RST_PORT;
-	pdata->irq_gpio = GTP_INT_PORT;
-	pdata->slide_wakeup = false;
-	pdata->auto_update = true;
-	pdata->auto_update_cfg = false;
-	pdata->type_a_report = false;
-	pdata->esd_protect = false;
-	pdata->max_touch_id = GTP_MAX_TOUCH_ID;
-	pdata->abs_size_x = GTP_DEFAULT_MAX_X;
-	pdata->abs_size_y = GTP_DEFAULT_MAX_Y;
-	pdata->max_touch_width = GTP_DEFAULT_MAX_WIDTH;
-	pdata->max_touch_pressure = GTP_DEFAULT_MAX_PRESSURE;
-}
-#endif  // NOTUSED
 
 #endif  // !TEST_INTERRUPT
