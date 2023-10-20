864f9f7228ef2c737cb38fa1b909064d83e788ca
Lee Lup Yuen <luppy@appkaki.com>
2023-10-20 17:26:06 +0800

    OpenSBI tested OK yay!

--- boards/risc-v/jh7110/star64/src/jh7110_appinit.c
+++ boards/risc-v/jh7110/star64/src/jh7110_appinit.c
@@ -28,6 +28,7 @@
 #include <stdio.h>
 #include <syslog.h>
 #include <errno.h>
+#include <debug.h>
 
 #include <nuttx/board.h>
 #include <nuttx/drivers/ramdisk.h>
@@ -164,4 +165,146 @@ void board_late_initialize(void)
   mount(NULL, "/proc", "procfs", 0, NULL);
 
 #endif
+
+  // Make an ecall to OpenSBI
+  int test_opensbi(void);
+  int ret = test_opensbi();
+  DEBUGASSERT(ret == OK);
 }
+// SBI Definitions
+// https://github.com/riscv-software-src/opensbi/blob/master/include/sbi/sbi_ecall_interface.h
+
+/* SBI Extension IDs */
+#define SBI_EXT_0_1_SET_TIMER			0x0
+#define SBI_EXT_0_1_CONSOLE_PUTCHAR		0x1
+#define SBI_EXT_0_1_CONSOLE_GETCHAR		0x2
+#define SBI_EXT_0_1_CLEAR_IPI			0x3
+#define SBI_EXT_0_1_SEND_IPI			0x4
+#define SBI_EXT_0_1_REMOTE_FENCE_I		0x5
+#define SBI_EXT_0_1_REMOTE_SFENCE_VMA		0x6
+#define SBI_EXT_0_1_REMOTE_SFENCE_VMA_ASID	0x7
+#define SBI_EXT_0_1_SHUTDOWN			0x8
+#define SBI_EXT_BASE				0x10
+#define SBI_EXT_TIME				0x54494D45
+#define SBI_EXT_IPI				0x735049
+#define SBI_EXT_RFENCE				0x52464E43
+#define SBI_EXT_HSM				0x48534D
+#define SBI_EXT_SRST				0x53525354
+#define SBI_EXT_PMU				0x504D55
+#define SBI_EXT_DBCN				0x4442434E
+#define SBI_EXT_SUSP				0x53555350
+#define SBI_EXT_CPPC				0x43505043
+
+/* SBI function IDs for DBCN extension */
+#define SBI_EXT_DBCN_CONSOLE_WRITE		0x0
+#define SBI_EXT_DBCN_CONSOLE_READ		0x1
+#define SBI_EXT_DBCN_CONSOLE_WRITE_BYTE		0x2
+
+/* SBI return error codes */
+#define SBI_SUCCESS				0
+#define SBI_ERR_FAILED				-1
+#define SBI_ERR_NOT_SUPPORTED			-2
+#define SBI_ERR_INVALID_PARAM			-3
+#define SBI_ERR_DENIED				-4
+#define SBI_ERR_INVALID_ADDRESS			-5
+#define SBI_ERR_ALREADY_AVAILABLE		-6
+#define SBI_ERR_ALREADY_STARTED			-7
+#define SBI_ERR_ALREADY_STOPPED			-8
+
+// SBI Return Type. From
+// https://github.com/riscv-software-src/opensbi/blob/master/firmware/payloads/test_main.c
+struct sbiret {
+	unsigned long error;
+	unsigned long value;
+};
+
+static struct sbiret sbi_ecall(unsigned int extid, unsigned int fid,
+                                  uintptr_t parm0, uintptr_t parm1,
+                                  uintptr_t parm2, uintptr_t parm3,
+                                  uintptr_t parm4, uintptr_t parm5);
+
+// Make an ecall to OpenSBI. Based on
+// https://github.com/riscv-software-src/opensbi/blob/master/firmware/payloads/test_main.c
+// https://www.thegoodpenguin.co.uk/blog/an-overview-of-opensbi/
+int test_opensbi(void)
+{
+  // Print `123` to Debug Console with Legacy Console Putchar.
+  // Call sbi_console_putchar: EID 0x01, FID 0
+  // https://github.com/riscv-non-isa/riscv-sbi-doc/blob/master/src/ext-legacy.adoc
+  sbi_ecall(SBI_EXT_0_1_CONSOLE_PUTCHAR, 0, '1', 0, 0, 0, 0, 0);
+  sbi_ecall(SBI_EXT_0_1_CONSOLE_PUTCHAR, 0, '2', 0, 0, 0, 0, 0);
+  sbi_ecall(SBI_EXT_0_1_CONSOLE_PUTCHAR, 0, '3', 0, 0, 0, 0, 0);
+
+  // TODO: Why does this return SBI_ERR_NOT_SUPPORTED?
+  // Print `456` to Debug Console.
+  // Call sbi_debug_console_write: EID 0x4442434E, FID 0
+  // https://github.com/riscv-non-isa/riscv-sbi-doc/blob/master/src/ext-debug-console.adoc#function-console-write-fid-0
+  const char *str = "456";
+	struct sbiret sret = sbi_ecall(
+    SBI_EXT_DBCN,  // Extension ID
+    SBI_EXT_DBCN_CONSOLE_WRITE,  // Function ID
+		strlen(str),         // Number of bytes
+    (unsigned long)str,  // Address Low
+    0,                   // Address High
+    0, 0, 0              // Unused
+  );
+  _info("sret.value=%d, sret.error=%d\n", sret.value, sret.error);
+  // DEBUGASSERT(sret.error == SBI_SUCCESS);
+  // DEBUGASSERT(sret.value == strlen(str));
+
+  // TODO: Why does this return SBI_ERR_NOT_SUPPORTED?
+  // Print `789` to Debug Console.
+  // Call sbi_debug_console_write_byte: EID 0x4442434E, FID 2
+  // https://github.com/riscv-non-isa/riscv-sbi-doc/blob/master/src/ext-debug-console.adoc#function-console-write-byte-fid-2
+  sret = sbi_ecall(SBI_EXT_DBCN, SBI_EXT_DBCN_CONSOLE_WRITE_BYTE, '7', 0, 0, 0, 0, 0);
+  sret = sbi_ecall(SBI_EXT_DBCN, SBI_EXT_DBCN_CONSOLE_WRITE_BYTE, '8', 0, 0, 0, 0, 0);
+  sret = sbi_ecall(SBI_EXT_DBCN, SBI_EXT_DBCN_CONSOLE_WRITE_BYTE, '9', 0, 0, 0, 0, 0);
+  _info("sret.value=%d, sret.error=%d\n", sret.value, sret.error);
+  // DEBUGASSERT(sret.error == SBI_SUCCESS);
+  // DEBUGASSERT(sret.value == strlen(str));
+
+  return OK;
+}
+
+// Make an ecall to OpenSBI. Based on
+// https://github.com/apache/nuttx/blob/master/arch/risc-v/src/common/supervisor/riscv_sbi.c#L52-L77
+// https://github.com/riscv-software-src/opensbi/blob/master/firmware/payloads/test_main.c
+static struct sbiret sbi_ecall(unsigned int extid, unsigned int fid,
+                                  uintptr_t parm0, uintptr_t parm1,
+                                  uintptr_t parm2, uintptr_t parm3,
+                                  uintptr_t parm4, uintptr_t parm5)
+{
+	struct sbiret ret;
+  register long r0 asm("a0") = (long)(parm0);
+  register long r1 asm("a1") = (long)(parm1);
+  register long r2 asm("a2") = (long)(parm2);
+  register long r3 asm("a3") = (long)(parm3);
+  register long r4 asm("a4") = (long)(parm4);
+  register long r5 asm("a5") = (long)(parm5);
+  register long r6 asm("a6") = (long)(fid);
+  register long r7 asm("a7") = (long)(extid);
+
+  asm volatile
+    (
+     "ecall"
+     : "+r"(r0), "+r"(r1)
+     : "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7)
+     : "memory"
+     );
+
+	ret.error = r0;
+	ret.value = r1;
+  return ret;
+}
+
+/* Output Log:
+Starting kernel ...
+
+clk u5_dw_i2c_clk_core already disabled
+clk u5_dw_i2c_clk_apb already disabled
+BC123test_opensbi: sret.value=0, sret.error=-2
+test_opensbi: sret.value=0, sret.error=-2
+
+NuttShell (NSH) NuttX-12.0.3
+nsh> 
+*/
