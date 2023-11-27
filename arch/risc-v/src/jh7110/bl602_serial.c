095a2567f65c4397b3ff99e910f21eee220afa7a
Lee Lup Yuen <luppy@appkaki.com>
2023-11-27 12:55:57 +0800

    Clean up UART. Still the same

--- arch/risc-v/src/jh7110/bl602_serial.c
+++ arch/risc-v/src/jh7110/bl602_serial.c
@@ -412,10 +412,6 @@ static int bl602_attach(struct uart_dev_s *dev)
   _info("BL602_UART_INT_MASK=0x%x\n", getreg32(BL602_UART_INT_MASK(0)));
   _info("BL602_UART_INT_CLEAR=0x%x\n", getreg32(BL602_UART_INT_CLEAR(0)));
   _info("BL602_UART_INT_EN=0x%x\n", getreg32(BL602_UART_INT_EN(0)));
-
-  // Clear RX FIFO
-  _info("BL602_UART_FIFO_CONFIG_0=0x%x\n", getreg32(BL602_UART_FIFO_CONFIG_0(0)));
-  putreg32(1 << 3, BL602_UART_FIFO_CONFIG_0(0));
   _info("BL602_UART_FIFO_CONFIG_0=0x%x\n", getreg32(BL602_UART_FIFO_CONFIG_0(0)));
 
   // Dump the UART and PLIC
