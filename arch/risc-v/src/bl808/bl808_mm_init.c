6d25105a7710c32fe8928039a025a74a040c7885
Lee Lup Yuen <luppy@appkaki.com>
2023-12-09 15:42:18 +0800

    Clean up. Tested OK

--- arch/risc-v/src/bl808/bl808_mm_init.c
+++ arch/risc-v/src/bl808/bl808_mm_init.c
@@ -36,33 +36,24 @@
 #include "riscv_internal.h"
 #include "riscv_mmu.h"
 
-//// Begin Test
-// From https://github.com/torvalds/linux/blob/master/arch/riscv/include/asm/pgtable-64.h#L126-L142
-/*
-* [63:59] T-Head Memory Type definitions:
-* bit[63] SO - Strong Order
-* bit[62] C - Cacheable
-* bit[61] B - Bufferable
-* bit[60] SH - Shareable
-* bit[59] Sec - Trustable
-* 00110 - NC   Weakly-ordered, Non-cacheable, Bufferable, Shareable, Non-trustable
-* 01110 - PMA  Weakly-ordered, Cacheable, Bufferable, Shareable, Non-trustable
-* 10010 - IO   Strongly-ordered, Non-cacheable, Non-bufferable, Shareable, Non-trustable
-*/
-// #define _PAGE_PMA_THEAD		((1UL << 62) | (1UL << 61) | (1UL << 60))
-// #define _PAGE_NOCACHE_THEAD	((1UL < 61) | (1UL << 60))
-#define MMU_THEAD_IO_FLAGS		((1UL << 63) | (1UL << 60))
-// #define _PAGE_MTMASK_THEAD	(_PAGE_PMA_THEAD | _PAGE_IO_THEAD | (1UL << 59))
-//// End Test
-
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
 
-/* Map the whole I/O memory with vaddr = paddr mappings */
+/* T-Head C906 MMU requires Strong Order and Shareable for I/O Memory */
 
-#define MMU_IO_BASE      (0x00000000)
-#define MMU_IO_SIZE      (0x40000000)
+#define MMU_THEAD_SHAREABLE    (1UL << 60)
+#define MMU_THEAD_STRONG_ORDER (1UL << 63)
+#define MMU_THEAD_IO_FLAGS		 (MMU_IO_FLAGS | MMU_THEAD_SHAREABLE | \
+                                MMU_THEAD_STRONG_ORDER)
+
+/* Map the I/O and PLIC Memory with vaddr = paddr mappings */
+
+#define MMU_IO_BASE      (0x00000000ul)
+#define MMU_IO_SIZE      (0x40000000ul)
+
+#define MMU_INT_BASE     (0xe0000000ul)
+#define MMU_INT_SIZE     (0x10000000ul)
 
 /* Physical and virtual addresses to page tables (vaddr = paddr mapping) */
 
@@ -254,16 +245,16 @@ void bl808_kernel_mappings(void)
 
   binfo("map I/O regions\n");
   mmu_ln_map_region(1, PGT_L1_VBASE, MMU_IO_BASE, MMU_IO_BASE,
-                    MMU_IO_SIZE, MMU_IO_FLAGS | MMU_THEAD_IO_FLAGS);
+                    MMU_IO_SIZE, MMU_THEAD_IO_FLAGS);
 
   // Map the PLIC for Interrupt L2
   binfo("map PLIC for Interrupt L2\n");
-  mmu_ln_map_region(2, PGT_L2_INT_PBASE, 0xE0000000, 0xE0000000, 0x10000000, ////TODO
-                    MMU_IO_FLAGS | MMU_THEAD_IO_FLAGS);
+  mmu_ln_map_region(2, PGT_L2_INT_PBASE, MMU_INT_BASE, MMU_INT_BASE,
+                    MMU_INT_SIZE, MMU_THEAD_IO_FLAGS);
 
   // Connect the L1 and Interrupt L2 page tables for PLIC
   binfo("connect the L1 and Interrupt L2 page tables for PLIC\n");
-  mmu_ln_setentry(1, PGT_L1_VBASE, PGT_L2_INT_PBASE, 0xE0000000, PTE_G); ////TODO
+  mmu_ln_setentry(1, PGT_L1_VBASE, PGT_L2_INT_PBASE, MMU_INT_BASE, PTE_G);
 
   /* Map the kernel text and data for L2/L3 */
 
