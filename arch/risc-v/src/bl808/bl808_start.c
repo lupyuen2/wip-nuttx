472cb72f1d603102a6596f43edba5da4408ec2c0
Lee Lup Yuen <luppy@appkaki.com>
2023-12-10 14:20:13 +0800

    Clean up. Tested OK

--- arch/risc-v/src/bl808/bl808_start.c
+++ arch/risc-v/src/bl808/bl808_start.c
@@ -186,12 +186,11 @@ void riscv_serialinit(void)
 
 static void bl808_copy_ramdisk(void)
 {
+  uint32_t size;
+
   // After _edata, search for "-rom1fs-". This is the RAM Disk Address.
   // Limit search to 256 KB after Idle Stack Top.
-  extern uint8_t _edata[];
-  extern uint8_t _sbss[];
-  extern uint8_t _ebss[];
-  _info("_edata=%p, _sbss=%p, _ebss=%p, BL808_IDLESTACK_TOP=%p\n", (void *)_edata, (void *)_sbss, (void *)_ebss, BL808_IDLESTACK_TOP);
+  _info("_edata=%p, _sbss=%p, _ebss=%p, BL808_IDLESTACK_TOP=%p\n", (void *)_edata, (void *)_sbss, (void *)_ebss, BL808_IDLESTACK_TOP);////
   const char *header = "-rom1fs-";
   uint8_t *ramdisk_addr = NULL;
   for (uint8_t *addr = _edata; addr < (uint8_t *)BL808_IDLESTACK_TOP + (256 * 1024); addr++)
@@ -204,26 +203,31 @@ static void bl808_copy_ramdisk(void)
     }
 
   // Stop if RAM Disk is missing
-  _info("ramdisk_addr=%p\n", ramdisk_addr);
-  if (ramdisk_addr == NULL) { _info("Missing RAM Disk. Check the initrd padding."); }
-  DEBUGASSERT(ramdisk_addr != NULL);
+  _info("ramdisk_addr=%p\n", ramdisk_addr);////
+  if (ramdisk_addr == NULL)
+    {
+      _info("Missing RAM Disk. Check the initrd padding.");
+      PANIC();
+    }
 
   // RAM Disk must be after Idle Stack, to prevent overwriting
-  if (ramdisk_addr <= (uint8_t *)BL808_IDLESTACK_TOP) { _info("RAM Disk must be after Idle Stack. Increase the initrd padding by %ul bytes.", (size_t)BL808_IDLESTACK_TOP - (size_t)ramdisk_addr); }
-  DEBUGASSERT(ramdisk_addr > (uint8_t *)BL808_IDLESTACK_TOP);
+  if (ramdisk_addr <= (uint8_t *)BL808_IDLESTACK_TOP)
+    {
+      _info("RAM Disk must be after Idle Stack. Increase initrd padding by %ul bytes.", (size_t)BL808_IDLESTACK_TOP - (size_t)ramdisk_addr);
+      PANIC();
+    }
 
   // Read the Filesystem Size from the next 4 bytes, in Big Endian
-  // Add 0x1F0 to Filesystem Size
-  const uint32_t size =
-    (ramdisk_addr[8] << 24) + 
-    (ramdisk_addr[9] << 16) + 
-    (ramdisk_addr[10] << 8) + 
-    ramdisk_addr[11] + 
-    0x1F0;
-  _info("size=%d\n", size);
+  size = (ramdisk_addr[8] << 24) + (ramdisk_addr[9] << 16) + 
+         (ramdisk_addr[10] << 8) + ramdisk_addr[11] + 0x1f0;
+  _info("size=%d\n", size);////
 
   // Filesystem Size must be less than RAM Disk Memory Region
-  DEBUGASSERT(size <= (size_t)__ramdisk_size);
+  if (size > (size_t)__ramdisk_size)
+  {
+    _info("RAM Disk Region too small. Increase by %ul bytes.\n", size - (size_t)__ramdisk_size);
+    PANIC();
+  }
 
   // Copy the Filesystem Size to RAM Disk Start
   // Warning: __ramdisk_start overlaps with ramdisk_addr + size
@@ -237,10 +241,15 @@ static void bl808_copy_overlap(uint8_t *dest, const uint8_t *src, size_t count)
   uint8_t *d = dest + count - 1;
   const uint8_t *s = src + count - 1;
 
-  DEBUGASSERT(dest > src); ////
+  if (dest <= src)
+    {
+      _info("dest and src should overlap");
+      PANIC();
+    }
+
   while (count--)
     {
-      volatile uint8_t c = *s;
+      volatile uint8_t c = *s;  /* Prevent compiler optimisation */
       *d = c;
       d--;
       s--;
