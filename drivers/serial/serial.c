d20234441f173fddf6faa1a6cd24a809467f8a48
Lee Lup Yuen <luppy@appkaki.com>
2024-01-11 15:00:23 +0800

    uart_read: buf[0]=a, recvd=1

--- drivers/serial/serial.c
+++ drivers/serial/serial.c
@@ -736,6 +736,7 @@ static int uart_close(FAR struct file *filep)
 static ssize_t uart_read(FAR struct file *filep,
                          FAR char *buffer, size_t buflen)
 {
+  FAR char *buffer0 = buffer;////
   FAR struct inode *inode = filep->f_inode;
   FAR uart_dev_t *dev = inode->i_private;
   FAR struct uart_buffer_s *rxbuf = &dev->recv;
@@ -803,6 +804,7 @@ static ssize_t uart_read(FAR struct file *filep,
           /* Take the next character from the tail of the buffer */
 
           ch = rxbuf->buffer[tail];
+          _info("ch=%c\n", ch);////
 
           /* Increment the tail index.  Most operations are done using the
            * local variable 'tail' so that the final rxbuf->tail update
@@ -1151,7 +1153,7 @@ static ssize_t uart_read(FAR struct file *filep,
 #endif
 
   nxmutex_unlock(&dev->recv.lock);
-  _info("buf[0]=%c, recvd=%d\n", buffer[0], recvd);////
+  _info("buf[0]=%c, recvd=%d\n", buffer0[0], recvd);////
   return recvd;
 }
 
