105d96681a8b4242e428fe76608dc3a2cec87c1e
Lee Lup Yuen <luppy@appkaki.com>
2024-03-07 16:51:09 +0800

    Log the setting of all Page Table Entries in MMU

--- arch/risc-v/src/common/riscv_mmu.c
+++ arch/risc-v/src/common/riscv_mmu.c
@@ -23,6 +23,7 @@
  ****************************************************************************/
 
 #include <assert.h>
+#include <debug.h> ////
 
 #include <nuttx/config.h>
 #include <nuttx/arch.h>
@@ -62,6 +63,7 @@ static const size_t g_pgt_sizes[] =
 void mmu_ln_setentry(uint32_t ptlevel, uintptr_t lnvaddr, uintptr_t paddr,
                      uintptr_t vaddr, uint64_t mmuflags)
 {
+  _info("ptlevel=%p, lnvaddr=%p, paddr=%p, vaddr=%p, mmuflags=%p\n", ptlevel, lnvaddr, paddr, vaddr, mmuflags); ////
   uintptr_t *lntable = (uintptr_t *)lnvaddr;
   uint32_t   index;
 
