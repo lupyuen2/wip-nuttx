a577835fa909cc48105d299e1288c4209f12cf32
Lup Yuen Lee <luppy@appkaki.com>
2024-07-02 15:11:42 +0800

    Clean up

--- boards/risc-v/bl808/ox64/src/bl808_appinit.c
+++ boards/risc-v/bl808/ox64/src/bl808_appinit.c
@@ -28,14 +28,16 @@
 #include <stdio.h>
 #include <syslog.h>
 #include <errno.h>
-#include <debug.h> ////
 
 #include <nuttx/board.h>
 #include <nuttx/drivers/ramdisk.h>
 #include <sys/mount.h>
 #include <sys/boardctl.h>
 #include <arch/board/board_memorymap.h>
-#include "bl808_gpio.h" ////
+
+#ifdef CONFIG_USERLED
+#include <nuttx/leds/userled.h>
+#endif
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -168,7 +170,6 @@ void board_late_initialize(void)
 #endif
 
 #ifdef CONFIG_USERLED
-  ////TODO: Move to bringup.c
   /* Register the LED driver */
 
   int ret = userled_lower_initialize("/dev/userleds");
@@ -177,19 +178,4 @@ void board_late_initialize(void)
       syslog(LOG_ERR, "ERROR: userled_lower_initialize() failed: %d\n", ret);
     }
 #endif
-
-  ////TODO
-  #define GPIO_PIN  29
-  #define GPIO_ATTR (GPIO_OUTPUT | GPIO_FUNC_SWGPIO)
-
-  // _info("Config GPIO: pin=%d, attr=0x%x\n", GPIO_PIN, GPIO_ATTR);
-  int ret2 = bl808_configgpio(GPIO_PIN, GPIO_ATTR);
-  DEBUGASSERT(ret2 == OK);
-
-  // _info("Set GPIO: pin=%d\n", GPIO_PIN);
-  bl808_gpiowrite(GPIO_PIN, true);
-  up_mdelay(1000);
-
-  // _info("Clear GPIO: pin=%d\n", GPIO_PIN);
-  bl808_gpiowrite(GPIO_PIN, false);
 }
