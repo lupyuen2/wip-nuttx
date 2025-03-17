19107b4f253d5c0f552eafc4a89fb8ede7254117
Lup Yuen Lee <luppy@appkaki.com>
2025-03-17 23:32:21 +0800

    MMU OK yay! write_sysreg(value | (1 << 2) | (1 << 12) | 1, sctlr_el1);
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- arch/arm64/src/common/arm64_mmu.c
+++ arch/arm64/src/common/arm64_mmu.c
@@ -615,11 +615,12 @@ static void enable_mmu_el1(unsigned int flags)
   /* Enable the MMU and data cache */
 
   value = read_sysreg(sctlr_el1);
-  write_sysreg((value | SCTLR_M_BIT
-#ifndef CONFIG_ARM64_DCACHE_DISABLE
-               | SCTLR_C_BIT
-#endif
-               ), sctlr_el1);
+//   write_sysreg((value | SCTLR_M_BIT
+// #ifndef CONFIG_ARM64_DCACHE_DISABLE
+//                | SCTLR_C_BIT
+// #endif
+//                ), sctlr_el1);
+  write_sysreg(value | (1 << 2) | (1 << 12) | 1, sctlr_el1); ////
 
   /* Ensure the MMU enable takes effect immediately */
 
