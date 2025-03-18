a64b412daaf463eaf2dff0b01f1e6ad5ea8cbd35
Lup Yuen Lee <luppy@appkaki.com>
2025-03-18 14:26:19 +0800

    Disable MMU debugging
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- arch/arm64/src/common/arm64_mmu.c
+++ arch/arm64/src/common/arm64_mmu.c
@@ -49,12 +49,12 @@
  * #define CONFIG_MMU_DUMP_PTE 1
  */
 
-#define CONFIG_MMU_ASSERT 1 ////
-#define CONFIG_MMU_DEBUG 1 ////
-#define CONFIG_MMU_DUMP_PTE 1 ////
-#define trace_printf _info ////
-#undef sinfo ////
-#define sinfo _info ////
+// #define CONFIG_MMU_ASSERT 1 ////
+// #define CONFIG_MMU_DEBUG 1 ////
+// #define CONFIG_MMU_DUMP_PTE 1 ////
+// #define trace_printf _info ////
+// #undef sinfo ////
+// #define sinfo _info ////
 
 #ifdef CONFIG_MMU_DEBUG
 
